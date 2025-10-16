0b57f0fcfc1ba15e944a96d83592e692cbb743bb
Jnpacheco-125 <rm350127@fiap.com.br>
2025-10-16 07:57:06 -0300

    primeiro commit

---
+++ src/test/java/br/com/dio/estoque_cerveja/EstoqueCervejaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.estoque_cerveja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueCervejaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
